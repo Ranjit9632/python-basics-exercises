b81cd865b7fd11b708055e9c931e22eeae2dccd7
Ranjit9632 <34565471+Ranjit9632@users.noreply.github.com>
2024-05-26 17:40:39 +0530

    Update 5-challenge-pick-apart-your-users-input.py
    this actually makes it look easier than assignment operator and the string methods extension shall suffice

--- ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
+++ ch04-strings-and-string-methods/5-challenge-pick-apart-your-users-input.py
@@ -5,5 +5,5 @@
 # Return the upper-case first letter entered by the user
 
 user_input = input("Tell me your password: ")
-first_letter = user_input[0]
-print("The first letter you entered was: " + first_letter.upper())
+upper_first_letter = user_input[0].upper()
+print("The first letter you entered was: ", upper_first_letter)
